eb635e6edf5c9f4360c9ee149aa0d602d41b6908
hack3r0n <rhom09@gmail.com>
2020-08-07 18:53:36 -0300

    Finalizando pagina de cadastro

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+let Chance = require('chance')
+let chance = new Chance();
+
 context('Cadastro', () => {
     it('cadastro de usuario no site', () => {
         cy.visit('Register.html');
@@ -9,5 +12,23 @@ context('Cadastro', () => {
         cy.get("textarea[ng-model='Adress']").type('Via Transversal Sul')
         cy.get('input[ng-model="EmailAdress"]').type('rhom0909@teste.com')
         cy.get('input[ng-model="Phone"]').type('959906029')
+
+        // checkbox
+        cy.get("input[value='Male']").check()
+        cy.get("input[type='checkbox']").check('Movies')
+
+        // select e select2
+        cy.get('#Skills').select('Javascript')
+        cy.get('#countries').select('Brazil')
+        cy.get('#country').select('Netherlands', { force: true})
+        cy.get('#yearbox').select('1987')
+        cy.get("select[ng-model='monthbox']").select('April')
+        cy.get('#daybox').select('8')
+
+        cy.get('#firstpassword').type('Abc123@')
+        cy.get('#secondpassword').type('Abc123@')
     });
 });
+
+
+

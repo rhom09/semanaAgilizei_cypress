f344479e90d6800cf263f6c96e6a271fa31a6120
hack3r0n <rhom09@gmail.com>
2020-08-10 12:54:15 -0300

    Criando relatórios com o mochawesome e cucumber_report

---
+++ cypress/reports/reporter.js
@@ -0,0 +1,19 @@
+const reporter = require('cucumber-html-reporter')
+
+const options = {
+    theme: 'bootstrap',
+    jsonDir: 'cypress/reports/cucumber-json',
+    output: 'cypress/reports/cucumber_report.html',
+    reportSuiteAsScenarios: true,
+    scenarioTimestamp: true,
+    launchReport: true,
+    metadata: {
+        "App Name": "Demo Automation",
+        "Test Environment": "STAGING",
+        "Browser": "Electron",
+        "Platform": "Linux Mint",
+        "Executed": "Local"
+    }
+};
+
+reporter.generate(options)
